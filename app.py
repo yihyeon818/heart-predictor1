30e27edae642b7b6b808a42c187a0f92fa80e655
yihyeon818 <162284817+yihyeon818@users.noreply.github.com>
2025-05-10 00:43:15 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1gzc40Fba1zTsOjuNLPm-NmB5xc8HFvu9
 """
 
-!pip install streamlit
+pip install streamlit
 
 import streamlit as st
 import pandas as pd
